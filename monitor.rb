49b5cb7de7a237fb941c1460f498bf9a0089e47f
Ryan McGeehan <billygoatmagoo@gmail.com>
2015-02-18 23:52:27 -0800

    Use Watchfile

--- monitor.rb
+++ monitor.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'file-tail'
 require 'ruby-growl'
 require 'json'
@@ -7,15 +9,5 @@ g = Growl.new "localhost", "ruby-growl"
 #OSQuery Results Location
 filename = '/var/log/osquery/osqueryd.results.log'
 
-#Tail the log and report to growl locally. 
-File.open(filename) do |log|
-  log.extend(File::Tail)
-  log.interval = 10
-  log.backward(0)
-  log.tail { |line|
-    alert = JSON.parse(line) 
-    g.add_notification "ruby-growl Notification"
-    g.notify "ruby-growl Notification", "OSQuery Alert", "#{alert['name']} changed. #{alert['columns']['name']} was #{alert['action']}.",0, true
-  }
-end
-
+g.add_notification "ruby-growl Notification"
+g.notify "ruby-growl Notification", "OSQuery Alert", "Logs happened that you should probably pay attention to. /var/log/osquery/osquery.log",0, true
